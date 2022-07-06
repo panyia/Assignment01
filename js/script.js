f79ce1bbef695bbb53603727cdd570a957880194
Your Name <you@example.com>
2022-07-06 00:24:02 -0700

    commiting assignment 1

---
+++ js/script.js
@@ -0,0 +1,152 @@
+// prompt('hello world');
+
+// STEP 1
+// let some_month; --> let someMonth;
+// function the Month(); --> function theMonth();
+// let current-month; --> let currentMonth;
+// let summer_month; --> let summerMonth;
+// let MyLibrary-function --> let myLibraryFunction;
+
+
+// STEP 2
+// Numeric literal example --> 5
+// String literal example --> 'Hello World'
+// Boolean literal expression --> false
+// null literal expression --> null
+
+
+// STEP 3
+// let numberOfPlayers = prompt('How many players would you like on the server?');
+// console.log(numberOfPlayers);
+
+
+// STEP 4
+// let firstName;
+// let strLastName;
+// var address;
+// var city;
+// let state;
+// let _zipCode;
+// let intAge;
+// let referralSource;
+// let $boolContact;
+
+
+// STEP 5
+// let firstName = 'Panyia';
+// let $boolContact = confirm('May we contact you?');
+// let intAge = prompt('How old are you?');
+
+
+// STEP 6
+// let age = 31;
+// console.log('I am ' + age + ' years old.');
+
+
+// STEP 7
+// let isCollegeGraduate = console.log('Is Panyia a college graduate? ' + true);
+// let totalNumber = console.log(2 + false);
+
+
+// STEP 8
+// The string is not valid.
+// Here is how I would fix it:
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+
+// STEP 9
+// const isItNull = null;
+// console.log(isItNull);
+
+// let undeclaredValue;
+// console.log(undeclaredValue);
+
+
+// STEP 10
+// let _string = console.log((typeof 'Hello World'));
+// let _number = console.log((typeof 7));
+// let _boolean = console.log((typeof true));
+// let _object = console.log((typeof null));
+// let _undefined = console.log((typeof newVariable));
+
+
+// STEP 11
+// alert('Hello ' + 'Panyia Fang' + ', welcome to the JavaScript class!');
+
+
+// STEP 12
+// let name = 'Panyia Fang';
+// alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+
+// STEP 13
+// let name = 'Panyia Fang';
+// let course = 'Javascript';
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+
+// STEP 14
+// let name = 'Panyia Fang';
+// let course = 'Javascript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+
+// STEP 15
+// let name = prompt('What is your full name?');
+// let course = 'Javascript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 16
+// let name = 'Panyia Fang';
+// let course = prompt('What class are you taking?');
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y);
+
+// STEP 18
+// let x = 20;
+// x = x+20;
+// console.log(x);
+
+
+// STEP 19
+// let x = 20;
+// x = x*5;
+// console.log(x);
+
+
+// STEP 20
+// let x = 20 % 3;
+// x /= 1;
+// console.log(x);
+
+
+// STEP 21
+// const areYouHungry = true;
+// const craveMexicanFood = true;
+
+// if ((areYouHungry) || (craveMexicanFood)) {
+//     console.log('YAY, LET\'S GO EAT MEXICAN FOOD!');
+// } else {
+//     console.log('Aww, maybe next time');
+// }
+
+
+// STEP 22
+// let firstName = prompt('What is your first name?');
+// let lastName = prompt('What is your last name?');
+
+// let accessFirstName = 'Panyia';
+// let accessLastName = 'Fang';
+
+// if ((firstName==accessFirstName) && (lastName==accessLastName)){
+//     console.log('Congratulations. Welcome to the club!pan');
+// } else {
+//     console.log('You have been denied access. Goodbye.');
+// }
